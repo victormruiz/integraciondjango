46df0124ececd301bff5e896c8c1165bb4ba4ea1
Victor Ruiz <victormruiz92@gmail.com>
2020-01-30 12:04:04 +0100

    probando

--- polls/tests.py
+++ polls/tests.py
@@ -43,7 +43,7 @@ def create_question(question_text, days):
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
-    time = timezone.now() + datetime.timedelta(days=0)
+    time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
